35321ab127e20fa20335ea22a5fcbeed82844ab7
Chris Hong <hongchristinaaa@gmail.com>
2023-01-30 03:06:24 -0800

    Can view user's saved foods

--- client/src/components/AllFood/FoodCards.js
+++ client/src/components/AllFood/FoodCards.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Card, Button } from 'react-bootstrap'
+import { OneSaved } from './OneSaved'
 
 export default function FoodCards ({userData, handleDeleteFood}) {
-    console.log(userData)
-
+    console.log(userData.savedFoods)
+    const [show, setShow] = useState({});
+    const handleClose = (id) => {
+        setShow((prevState) => ({ ...prevState, [id]: false }));
+    };
+    const handleShow = (id) => {
+        setShow((prevState) => ({ ...prevState, [id]: true }));
+    }
     return (
         <div>
-            {userData && userData.savedFood && userData.savedFood.map((savedFood, index) => ( 
-                <Card key={index}>
+            {userData && userData.savedFoods && userData.savedFoods.map((savedFood, index) => ( 
+                <Card key={index} className='card col-xl-3 col-md-5 col-sm-8 col-xs-12' style={{margin:'2%'}}>
                     <Card.Title>{savedFood.name}</Card.Title>
-                    <Button>More Info</Button>
-                    <Button onClick={() => handleDeleteFood(savedFood.id)}>Delete</Button>
+                    <Card.Img src={savedFood.image_url} alt='' style={{height:'30%'}}/>
+                    <Card.Body>
+                        <Button onClick={() => handleShow(savedFood.id)}>More Info</Button>
+                        <Button onClick={() => handleDeleteFood(savedFood.id)}>Delete</Button>
+                        <OneSaved 
+                            show={show}
+                            onHide={() => handleClose(savedFood.id)}
+                            onClick={() => handleClose(savedFood.id)}
+                            handleClose={handleClose}
+                            savedFood={savedFood}
+                            handleDeleteFood={handleDeleteFood}
+                        />
+                    </Card.Body>
                 </Card>
             ))}
         </div>
---
+++ client/src/components/AllFood/OneSaved.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button'
+// import Carousel from 'react-bootstrap/Carousel';
+// import Moment from 'react-moment';
+
+export const OneSaved = ({savedFood, show, handleDeleteFood, handleClose}) => {
+    // console.log(data)
+    // console.log(id)
+    // console.log(review.reviews);
+  
+    return (
+        <Modal
+            {...savedFood}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            key={savedFood.id}
+            show={show[savedFood.id]}
+            onHide={() => handleClose(savedFood.id)}
+        >
+            <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+                {savedFood.name}
+            </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {/* <Carousel fade key={id.photos}>
+                    {id?.photos?.map((photos) =>
+                        <Carousel.Item >
+                        <img src={photos} alt='' />
+                        </Carousel.Item>
+                    )}
+
+                </Carousel> */}
+
+                {/* <div key={review.reviews}>
+                    {review?.reviews?.map((reviews) =>
+                        <div>
+                            <div className='row'>
+                                <img src={reviews.user.image_url} 
+                                alt='user pic' 
+                                style={{height:'100px', width:'100px', borderRadius:'50%'}}/>
+                                <div className='col-9'>
+                                    <p >{reviews.text}</p>
+                                    <Moment format="MM/DD/YYYY">{reviews.time_created}</Moment>
+
+                                </div>
+                            </div>
+                            <div className='row'>
+                                <p className='col-3'>- {reviews.user.name}</p>
+                                <p className='col-3'>Rating: {reviews.rating}</p>
+                            </div>
+                        </div>
+                    )}
+
+                </div> */}
+            </Modal.Body>
+            <Modal.Footer>
+            <Button onClick={() => handleClose(savedFood.id)}>Close</Button>
+            <Button onClick={() => handleDeleteFood(savedFood.id)}>Save</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
