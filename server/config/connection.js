c9d4e483309e6d3941d320547c6763e9ca49a42a
Chris Hong <hongchristinaaa@gmail.com>
2023-02-02 02:17:42 -0800

    updating connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,6 +7,8 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/foodgenie', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
 }) 
 
 module.exports = mongoose.connection;
