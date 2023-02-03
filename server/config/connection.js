f0fdfae2574a2b896b88a7790497673af930498b
Chris Hong <hongchristinaaa@gmail.com>
2023-02-02 17:09:48 -0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,11 +4,11 @@ require('dotenv').config({ path: path.resolve(__dirname, '..', '..', '.env') });
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGODB_URI || MONGODB_URI || 'mongodb://localhost:27017/foodgenie', {
+mongoose.connect(process.env.MONGODB_URI || MONGODB_URI || 'mongodb://localhost:27017/food-genie', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
   // useFindAndModify: true,
 }) 
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection; 
