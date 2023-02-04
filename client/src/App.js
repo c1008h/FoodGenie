45d831c80796fe723b357b69fe0c47f62ffaf7ca
Chris Hong <hongchristinaaa@gmail.com>
2023-02-04 02:48:59 -0800

    updated uri

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,8 @@ import Error from './pages/Error'
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-    uri: 'http://localhost:3006/graphql' || '/graphql'
+    uri: '/graphql' || 'https://foodgenie-ch.herokuapp.com/'
+    // 'http://localhost:3006/graphql' 
     // uri:'/graphql'
 });
 
