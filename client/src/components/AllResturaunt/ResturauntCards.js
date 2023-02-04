a877f3d47a9ae419a20afedad89d07097c5d02d6
Chris Hong <hongchristinaaa@gmail.com>
2023-02-03 17:03:21 -0800

    deploying

--- client/src/components/AllResturaunt/ResturauntCards.js
+++ client/src/components/AllResturaunt/ResturauntCards.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { Card, Button } from 'react-bootstrap'
-import { OneSaved } from '../AllResturaunt/OneSaved'
+import { OneSaved } from './OneSaved'
 import { authService } from '../../utils/auth';
 
 export default function ResturauntCards ({userData, handleDeleteResturaunt}) {
