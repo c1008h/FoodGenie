b35bfb27ec2deb200f4e848192a6955be015425a
Chris Hong <hongchris97@gmail.com>
2023-02-06 00:42:15 -0800

    added httpendpoint to server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const cors = require('cors')
-// require('dotenv').config();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3006;
 const app = express();
