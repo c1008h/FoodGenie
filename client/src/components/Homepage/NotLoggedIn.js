577562c972c6e7d6769e9d72608d349e1e100bbd
Chris Hong <hongchris97@gmail.com>
2023-08-20 02:30:25 -0700

    updated navbar and footer color

--- client/src/components/Homepage/NotLoggedIn.js
+++ client/src/components/Homepage/NotLoggedIn.js
@@ -1,35 +1,66 @@
 import React from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import { Container, Card, Row, Col, Button } from 'react-bootstrap';
 
 export default function NotLoggedIn() {
 
     return (
-        <Container style={{justifyContent:'center', textAlign:'center', marginTop:'2%'}}>
-            <h2>Welcome to the food Genie!</h2>
-            <p>The food genie is the solution to your daily food decision dilemma. 
-                Say goodbye to the time and energy wasted on choosing what to eat. 
-                With this app, you can save all your favorite foods and restaurants in one place, and let the genie do the work for you. 
-                Simply press the "food" or "resturaunt" button and the genie will randomly select a resturaunt or a meal for you from your saved options. 
-                No more indecision or boring meals. 
-                Create a account now and start enjoying effortless meal choices today!
-            </p>
-            <div className='row' style={{justifyContent:'center'}}>
-                <div className='col-3 mx-1'>
-                    <Card>
-                        <Card.Title>Up to 3 wishes per day!</Card.Title>
-                    </Card>
-                </div>
-                <div className='col-3 mx-1'>                
-                    <Card>
-                    <Card.Title>Search and view information about resturaunts!</Card.Title>
-                    </Card>
-                </div>
-                <div className='col-3 mx-1'>
-                    <Card>
-                        <Card.Title>Save time!</Card.Title>
-                    </Card>
-                </div>
-            </div>
+        <Container fluid='true' style={{justifyContent:'center', textAlign:'center', marginTop:'2%', padding:'5%'}}>
+            <Row>
+                <Col>
+                    <h2>Stop Wasting Time Deciding What to Eat</h2>
+                    <p>Food Genie helps you discover your next culinary adventure quickly and effortlessly.</p>
+                    <Link to='/signup'><Button>Get Started</Button></Link>
+                </Col>
+            </Row>
+            <Row>
+                <Container style={{display:'flex'}}>
+                    <Col>
+                        <Card>
+                            <h3>Restaurant Search</h3>
+                            <p>Explore a world of dining options with our easy-to-use restaurant search.</p>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <h3>Food Search</h3>
+                            <p>Craving something specific? Find restaurants serving your favorite dishes.</p>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <h3>Favorites</h3>
+                            <p>Save and organize your go-to restaurants and must-try meals effortlessly.</p>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <h3>Randomizer</h3>
+                            <p>Can't decide? Let Food Genie choose for you and embark on a spontaneous food adventure!</p>
+                        </Card>
+                    </Col>
+                </Container>
+            </Row>
+            <Row>
+                <Container style={{display:'flex'}}>
+                    <Col>
+                        <blockquote>"Food Genie has transformed my dining experience. I love the randomizer feature!"</blockquote>
+                        <p>- Sarah Johnson</p>
+                    </Col>
+                    <Col>
+                        <blockquote>"This app is a game-changer. It saves me so much time, and I've discovered amazing places to eat!"</blockquote>
+                        <p>- Michael Smith</p>
+                    </Col>
+                    <Col>
+                        <blockquote>"Food Genie has transformed my dining experience. I love the randomizer feature!"</blockquote>
+                        <p>- Johnson Sarah</p>
+                    </Col>
+                    <Col>
+                        <blockquote>"Food Genie has transformed my dining experience. I love the randomizer feature!"</blockquote>
+                        <p>- Smith Michael</p>
+                    </Col>
+                </Container>
+            </Row>
         </Container>
     )
 }
